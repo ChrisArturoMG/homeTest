95af68edd1c03de155c2482349d749ff4ab18019
ChrisArturoMG <chrism.g@outlook.com>
2021-12-03 15:37:14 -0600

    nodemodules

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express();
 
+const dfff = requiere('dialogflow-fulfillment')
+
 app.get('/', (req, res) =>{
   res.send('todo bien')
+})
+
+app.post('/',express.json(),(req, res)=>{
+  const agent = new dfff.WebhookClient({
+    request : req,
+    response : res
+  });
+
+  function demo(agent){
+    agent.add(' Enviando respuesta desde el server ')
+  }
+
+  var intentMap = new Map();
+  intentMap.set('demo', demo)
+
+  agent.handleRequest(intentMap)
 })
 
 app.listen(process.env.PORT, ()=> console.log('server por el puerto 3333'))
