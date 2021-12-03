9877ef703949223a5d4e0fd53714cb27e93a41a6
ChrisArturoMG <chrism.g@outlook.com>
2021-12-03 12:37:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ app.get('/', (req, res) =>{
   res.send('todo bien')
 })
 
-app.listen(3333, ()=> console.log('server por el puerto 3333'))
+app.listen( ()=> console.log('server por el puerto 3333'))
