ce90a27b0bba1d709f318af1a34a15f10cfad00c
ChrisArturoMG <chrism.g@outlook.com>
2021-12-04 02:05:16 -0600

    Update app.js

--- app.js
+++ app.js
@@ -12,11 +12,27 @@ app.post('/',express.json(),(req, res)=>{
     response : res
   });
   
-  const  demo = (agent)=>{
+  const  demo =  (agent)=>{
     const  { planta } = agent.parameters;
-    const dialogo = `Voy a revisar, listo, tu planta ${planta} tiene de temperatura 10, humeda 5 y luz 8`;
-    agent.add( dialogo )
-  }
+    const idBluetooth = '30:ae:a4:99:49:aa';
+    let temp, hum, luz, ph;
+
+    let dialogo;
+
+    try {
+      const datos = mysqlConnection.query('SELECT * FROM lecturaNodo WHERE registerDate = (SELECT MAX(registerDate) FROM lecturaNodo WHERE idBluetooth = ? );', [ idBluetooth ], rows = (err, rows, fields) =>{  
+          try {
+          console.log('haciendo consulta')
+           dialogo = `Voy a revisar, listo, tu planta ${planta} tiene de temperatura ${rows[0].temperatura}`;
+           console.log(dialogo)
+
+          } catch (error) {
+            console.log(error)  
+          }
+      });
+    } catch (error) {
+      console.log(error)
+    }
 
   function customPayloadDemo(agent){
     var payloadData = {
