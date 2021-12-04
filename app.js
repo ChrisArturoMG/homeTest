128904588244a0d8b0ed5e3bf38165d8dbc9e368
ChrisArturoMG <chrism.g@outlook.com>
2021-12-03 21:20:41 -0600

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post('/',express.json(),(req, res)=>{
           return console.log(' no se encontro')
         }
 
-        console.log(datos)
+        console.log(datos._results)
         dialogo = `Voy a revisar, listo, tu planta ${planta} tiene de temperatura ${temp}, de humedad ${hum} y de luz ${luz}`;
         agent.add( dialogo )
       });
