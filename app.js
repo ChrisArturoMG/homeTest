80eaa40a2da2bd98e411f480f8e0f4222c24509d
ChrisArturoMG <chrism.g@outlook.com>
2021-12-03 21:53:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -11,25 +11,54 @@ app.get('/', (req, res) =>{
 })
 
 app.post('/',express.json(),(req, res)=>{
-  console.log('esta es')
-  res.send({
-    "speech": "<speak> The weather in is <say-as interpret-as='number'> degrees Farenheit </say-as> </speak>",  // ASCII characters only
-    "displayText": "The weather in  is ",
-    "data": {
-      "google": {
-        "expect_user_response": true,
-        "is_ssml": true,
-        "permissions_request": {
-          "opt_context": "...",
-          "permissions": [
-            "NAME",
-            "DEVICE_COARSE_LOCATION",
-            "DEVICE_PRECISE_LOCATION"
-          ]
-        }
-      }
-    }
+  const agent = new dfff.WebhookClient({
+    request : req,
+    response : res
   });
+
+  const  demo = async (agent)=>{
+    const  { planta } = agent.parameters;
+    const idBluetooth = '30:ae:a4:99:49:aa';
+    let temp, hum, luz, ph;
+    try {
+      const datos = await mysqlConnection.query('SELECT * FROM lecturaNodo WHERE registerDate = (SELECT MAX(registerDate) FROM lecturaNodo WHERE idBluetooth = ? );', [ idBluetooth ], (err, rows, fields) =>{  
+        console.log(datos._results)
+});
+      console.log(datos)
+      dialogo = `Voy a revisar, listo, tu planta ${planta} tiene de temperatura ${temp}, de humedad ${hum} y de luz ${luz}`;
+      agent.add( dialogo )
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  function customPayloadDemo(agent){
+    var payloadData = {
+      "richContent":[
+        [
+          {
+            "type" : "accordion",
+            "title": "Accordion title",
+            "subtitle": "Accordion subtitle",
+            "image" : {
+              "src": {
+                "rawUrl": "https://example.com/images/logo.png"
+              }
+            },
+            "text": "According text"
+          }
+        ]
+      ]
+    }
+    agent.add( new dfff.Payload(platform.UNSPECIFIED, payloadData, { sendAsMessage: true, rawPayload: true}))
+  }
+
+  var intentMap = new Map();
+  intentMap.set('demo', demo);
+  intentMap.set('customPayloadDemo', customPayloadDemo)
+
+  agent.handleRequest(intentMap)
 })
 
 app.listen(process.env.PORT, ()=> console.log('server por el puerto 3333'))
