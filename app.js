8a6db2a33dfb166cfab0e9b2b198706bc983505b
ChrisArturoMG <chrism.g@outlook.com>
2021-12-05 15:40:25 -0600

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post('/',express.json(), async (req, res)=>{
           mysqlConnection.query('SELECT * FROM lecturaNodo WHERE registerDate = (SELECT MAX(registerDate) FROM lecturaNodo WHERE idBluetooth = ? );', [ idBluetooth ], rows = (err, rows, fields) =>{  
             console.log('haciendo consulta')
             console.log(rows)
-            dialogo = `Hola ${rows.username} 
+            dialogo = `Hola ${usuario.username} 
             Ire a revisar! listo! tu planta ${planta}, tiene de temperatura ${rows[0].temperatura}, vamos a ver que mas tenemos por aqui, veo que la humedad es de ${rows[0].humedad}%, vaya! interesante! la luz es de ${rows[0].luz} y el ph es de ${rows[0].ph}`;
             
             console.log(dialogo)
