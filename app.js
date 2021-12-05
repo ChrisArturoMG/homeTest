8777294f806ad9285bb35de73a5786f548918c19
ChrisArturoMG <chrism.g@outlook.com>
2021-12-05 14:31:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -37,15 +37,15 @@ app.post('/',express.json(), async (req, res)=>{
             const idBluetooth = '30:ae:a4:99:49:aa';
             mysqlConnection.query('SELECT * FROM lecturaNodo WHERE registerDate = (SELECT MAX(registerDate) FROM lecturaNodo WHERE idBluetooth = ? );', [ idBluetooth ], rows = (err, rows, fields) =>{  
               console.log(rows)
+              dialogo = `${payload.name}
+              Ire a reviar! listo! tu planta ${planta}, tiene de temperatura ${rows[0].temperatura}, vamos a ver que mas tenemos por aqui, veo que la humedad es de ${rows[0].humedad}%, vaya! interesante! la luz es de ${rows[0].luz} y el ph es de ${rows[0].ph}`;
+              
+              console.log(dialogo)
+              
+              const  demo =  (agent)=>{
+                return agent.add( dialogo );
+              }
               try {
-                dialogo = `${payload.name}
-                Ire a reviar! listo! tu planta ${planta}, tiene de temperatura ${rows[0].temperatura}, vamos a ver que mas tenemos por aqui, veo que la humedad es de ${rows[0].humedad}%, vaya! interesante! la luz es de ${rows[0].luz} y el ph es de ${rows[0].ph}`;
-                
-                console.log(dialogo)
-                
-                const  demo =  (agent)=>{
-                  return agent.add( dialogo );
-                }
                 function customPayloadDemo(agent){
                   var payloadData = {
                     "richContent":[
