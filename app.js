b8a1e1edf0fb31f6f867c3cd273805f789d225af
ChrisArturoMG <chrism.g@outlook.com>
2021-12-05 14:59:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const dfff = require('dialogflow-fulfillment')
 const { mysqlConnection }= require('./database')
 
 app.post('/',express.json(),(req, res)=>{
-  const agent = new dfff.WebhookClient({ request : req, response : res });
   
   try {
+    const agent = new dfff.WebhookClient({ request : req, response : res });
 
     const  { planta } = agent.parameters;
   
