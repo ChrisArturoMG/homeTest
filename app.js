26845e515128581cd05eec6be9acd71b047786c0
ChrisArturoMG <chrism.g@outlook.com>
2021-12-05 15:11:49 -0600

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ async function verify(token) {
       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
   });
   const payload = ticket.getPayload();
-  console.log(payload)
+  //console.log(payload)
   return payload
 }
 app.post('/',express.json(),(req, res)=>{
