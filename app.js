9df98cf98aa5aee3b5328bb7559948febe5741b7
ChrisArturoMG <chrism.g@outlook.com>
2021-12-13 02:43:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -216,7 +216,7 @@ app.post('/',express.json(), async (req, res)=>{
 
                 for (let i = 0; i < rows.length; i++) {
                   dialogo = dialogo +  ` 
-                  Tu ${ nodos.alias } ${ i+1 }, tiene de temperatura ${rows[i].temperatura}, 
+                  Tu ${ nodos[i].alias } ${ i+1 }, tiene de temperatura ${rows[i].temperatura}, 
                   revisemos mas, veo que la humedad es de ${rows[i].humedad}%, 
                   la luz es de ${rows[0].luz} 
                   y el ph es de ${rows[0].ph}.`;
